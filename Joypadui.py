f76a5e0d9b40c049aeac5dd3ff658c2a6c33d6cd
kris sum <kris@switchsystems.co.uk>
2014-11-22 17:30:54 +0000

    started implementing filesystem searching

--- Joypadui.py
+++ Joypadui.py
@@ -1,22 +1,53 @@
-# UI class
 from Tkinter import *
 from PIL import Image, ImageTk
+import glob
 import tkFont
+import sys
 
 class Joypadui:
     'UI controller for joypad voting system'
     
     # count down timer - change this value if you want
     timerSeconds = 300
+    # how long to spend on the vote result screen
+    timeOnVoteResults = 120
 
-    # init using the root Tk() instance, and the Joypadio library
+    # init using the root Tk() instance and the Joypadio library
     def __init__(self, root, io):
         self.root = root
         self.io = io
-        self.timeRemaining = self.timerSeconds
-        self.canvas_height = root.winfo_screenheight()-40
-        self.canvas_width  = root.winfo_screenwidth()
-        self.font_header = tkFont.Font(family="Helvetica", size=100, weight="bold")
+        self.timeRemaining  = self.timerSeconds
+        self.canvas_height  = root.winfo_screenheight() - 40 
+        self.canvas_width   = root.winfo_screenwidth()
+        self.font_header    = tkFont.Font(family="Helvetica", size=100, weight="bold")
+        self.font_header2   = tkFont.Font(family="Helvetica", size=50, weight="bold")
+        #vote control
+        self.currentVoteId  = 1
+        self.voteConfig     = []
+   
+    # find files from the filesystem that will configure the vote system
+    # file structure should be as follows:
+    #
+    # resourecs/1a.gif/jpg      - left picture
+    # resoucres/1b.gif/jpg      - right picture
+    # resoucres/1a.txt          - left game name (title)
+    # resources/1b.txt          - right game name (title)
+    # resources/2..
+    # etc
+    #
+    def initFilesystem(self):
+        for i in xrange(1,16,1):
+            voteEntity = {
+                'index'         : i,
+                'mediaTypeA'    : 'jpg',
+                'mediaTypeB'    : 'jpg',
+                'headingA'      : 'Game A',
+                'headingB'      : 'Game B',
+            }
+            # look for files on filesystem
+            # add to self.voteConfig
+            
+            
 
     def initGui(self):
         self.c = Canvas(self.root, width=self.canvas_width, height=self.canvas_height)
@@ -24,19 +55,30 @@ class Joypadui:
         # make everything centered, that way we can deal with different resolutions easier insetad of
         # anchoring everything to the top left
         self.imageBG = Image.open( "resources/bg.jpg")
-  
         self.photoBG = ImageTk.PhotoImage(self.imageBG)
         self.bg = self.c.create_image(self.canvas_width/2,self.canvas_height/2,image=self.photoBG)
 
-        self.textTeamA = self.c.create_text(self.canvas_width/4, 100, text="Team A", font=self.font_header, fill="white")
-        self.textTeamB = self.c.create_text(self.canvas_width/4*3, 100, text="Team B", font=self.font_header, fill="white")
-        self.textTeamAscore = self.c.create_text(self.canvas_width/4, 250, text="0", font=self.font_header, fill="white")
-        self.textTeamBscore = self.c.create_text(self.canvas_width/4*3, 250, text="0", font=self.font_header, fill="white")
+        self.textHeadingA = self.c.create_text(self.canvas_width/4, 100, text="Game A", font=self.font_header2, fill="white")
+        self.textHeadingB = self.c.create_text(self.canvas_width/4*3, 100, text="Game B", font=self.font_header2, fill="white")
+        self.textTeamAscore = self.c.create_text(self.canvas_width/4, self.canvas_height - 150, text="0", font=self.font_header, fill="white")
+        self.textTeamBscore = self.c.create_text(self.canvas_width/4*3, self.canvas_height - 150, text="0", font=self.font_header, fill="white")
 
-        self.textTimer = self.c.create_text(self.canvas_width/2, self.canvas_height/2, text="COUNTDOWN", font=self.font_header, fill="red")
+        self.textTimer = self.c.create_text(self.canvas_width/2, 100, text="COUNTDOWN", font=self.font_header, fill="green")
 
+
+        #game A image
+        self.gif1 = anim_gif('resources/1.gif')
+        self.imageGame1 = self.c.create_image(self.canvas_width/4,self.canvas_height/2,image=None)
+
+        #game B image
+        self.gif2 = anim_gif('resources/2.gif')
+        self.imageGame2 = self.c.create_image(self.canvas_width/4*3,self.canvas_height/2,image=None)
+       
+        # start the GUI update routines
         self.updateUI()
         self.countdownTimer()
+        self.animate1()
+        self.animate2()
 
         self.c.pack()
 
@@ -50,7 +92,7 @@ class Joypadui:
     def resetCountdownTimer(self):
         self.timeRemaining = self.timerSeconds
 
-    def timerReacher(self):
+    def timerReached(self):
         'something'
         # dislpay the voting results screen, plus a countdown to the next vote session
 
@@ -60,3 +102,47 @@ class Joypadui:
         self.c.itemconfig(self.textTeamBscore, text = self.io.scoreB)
         self.c.itemconfig(self.textTimer, text= self.timeRemaining);
         self.root.after(200,self.updateUI)
+        
+    def animate1(self):
+        self.c.itemconfig(self.imageGame1, image= self.gif1['frames'][self.gif1['loc']])
+        self.gif1['loc'] += 1
+        if self.gif1['loc'] == self.gif1['len']:
+            self.gif1['loc'] = 0
+        self.root.after(self.gif1["delays"][self.gif1['loc']] - 20, self.animate1)
+        
+    def animate2(self):
+        self.c.itemconfig(self.imageGame2, image= self.gif2['frames'][self.gif2['loc']])
+        self.gif2['loc'] += 1
+        if self.gif2['loc'] == self.gif2['len']:
+            self.gif2['loc'] = 0
+        self.root.after(self.gif2["delays"][self.gif2['loc']] - 20, self.animate2)
+
+        
+#
+# other utility functinos
+#
+
+# anim_gif
+# modified code from http://stackoverflow.com/questions/17223854/displaying-animated-gifs-in-tkinter-using-pil
+def anim_gif(name):
+    
+    im = Image.open(name)
+    gif = { 'frames': [],
+            'delays': [],
+            'loc' : 0,
+            'len' : 0 }
+    pics = []
+    try:
+        while True:
+            pics.append(im.copy())
+            im.seek(len(pics))
+    except EOFError: pass
+    gif['frames'] = [ImageTk.PhotoImage(frame.convert('RGBA')) for frame in pics];
+    # each frame can have a different animatino duration as well
+    try:
+        gif['delays'] = [frame.info['duration'] for frame in pics];
+    except:
+        gif['delays'] = []  
+    
+    gif['len'] = len(gif['frames'])
+    return gif

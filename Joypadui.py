b150c35be8acd7cf8ccae605588994a121c27fb8
Kris Sum <Kris@switchsystems.co.uk>
2014-11-26 18:44:31 +0000

    added winner screens

--- Joypadui.py
+++ Joypadui.py
@@ -4,6 +4,7 @@ import glob
 import tkFont
 import sys
 import pprint
+import os
 
 class Joypadui:
     'UI controller for joypad voting system'
@@ -11,12 +12,14 @@ class Joypadui:
     # count down timer - change this value if you want
     timerSeconds = 10
     # how long to spend on the vote result screen
-    timeOnVoteResults = 120
+    timeOnVoteResults = 10
 
     # internal status, 0 = initialising, 1 = running a vote, 2 = displaying vote results,
     #   3 = sudden death mode (next vote wins!)
     # animations only run in state 1 or 2
     status = 0    
+    
+    heading_top = 125
 
     # init using the root Tk() instance and the Joypadio library
     def __init__(self, root, io):
@@ -24,6 +27,7 @@ class Joypadui:
         self.root = root
         self.io = io
         self.timeRemaining  = self.timerSeconds
+        self.displayTimeout = self.timeOnVoteResults
         self.canvas_height  = root.winfo_screenheight() - 40 
         self.canvas_width   = root.winfo_screenwidth()
         self.font_header    = tkFont.Font(family="Helvetica", size=100, weight="bold")
@@ -53,19 +57,22 @@ class Joypadui:
                 'heading_a'      : 'Game A',
                 'heading_b'      : 'Game B',
             }
+            
+            resdir = "resources" + os.path.sep
+            
             # look for files on filesystem
-            files = glob.glob("resources/" + str(i) + "*.*")
+            files = glob.glob(resdir + str(i) + "*.*")
             
             for team in ['a','b']:
-                if ("resources/" + str(i) + team + '.txt') in files:
-                    tmp = open("resources/" + str(i) + team + ".txt")
+                if (resdir + str(i) + team + '.txt') in files:
+                    tmp = open(resdir + str(i) + team + ".txt")
                     voteEntity['heading_' + team] = tmp.read()
-                if ("resources/" + str(i) + team +'.gif') in files:
-                    voteEntity['media_'+team] = "resources/" + str(i) + team +".gif"
-                if ("resources/" + str(i) + team +'.jpg') in files:
-                    voteEntity['media_'+team] = "resources/" + str(i) + team + ".jpg"   
-                if ("resources/" + str(i) + team +'.mp4') in files:
-                    voteEntity['media_'+team] = "resources/" + str(i) + team + ".mp4"
+                if (resdir + str(i) + team +'.gif') in files:
+                    voteEntity['media_'+team] = resdir + str(i) + team +".gif"
+                if (resdir + str(i) + team +'.jpg') in files:
+                    voteEntity['media_'+team] = resdir + str(i) + team + ".jpg"   
+                if (resdir + str(i) + team +'.mp4') in files:
+                    voteEntity['media_'+team] = resdir + str(i) + team + ".mp4"
                     
             if ((voteEntity['media_a']) and (voteEntity['media_b'])): 
                 # add to self.voteConfig
@@ -83,19 +90,28 @@ class Joypadui:
         self.imageBG = Image.open( "resources/bg.jpg")
         self.photoBG = ImageTk.PhotoImage(self.imageBG)
 
-        #self.imageBG_win_a = Image.open( "resources/bg_win_a.jpg")
-        #self.photoBG_win_a = ImageTk.PhotoImage(self.imageBG_win_a)
-        #self.imageBG_win_b = Image.open( "resources/bg_win_b.jpg")
-        #self.photoBG_win_b= ImageTk.PhotoImage(self.imageBG_win_b)
+        self.imageBG_win_a = Image.open( "resources/bg_win_a.jpg")
+        self.photoBG_win_a = ImageTk.PhotoImage(self.imageBG_win_a)
+        self.imageBG_win_b = Image.open( "resources/bg_win_b.jpg")
+        self.photoBG_win_b = ImageTk.PhotoImage(self.imageBG_win_b)
         
         self.bg = self.c.create_image(self.canvas_width/2,self.canvas_height/2,image=self.photoBG)
 
-        self.textHeadingA = self.c.create_text(self.canvas_width/4, 125, text="Game A", font=self.font_header2, fill="white", anchor="n", justify="center")
-        self.textHeadingB = self.c.create_text(self.canvas_width/4*3, 125, text="Game B", font=self.font_header2, fill="white", anchor="n", justify="center")
+        self.textHeadingA = self.c.create_text(self.canvas_width/4, self.heading_top, text="Game A", font=self.font_header2, fill="white", anchor="n", justify="center")
+        self.textHeadingB = self.c.create_text(self.canvas_width/4*3, self.heading_top, text="Game B", font=self.font_header2, fill="white", anchor="n", justify="center")
         self.textTeamAscore = self.c.create_text(self.canvas_width/4, self.canvas_height - 150, text="0", font=self.font_header, fill="white", justify="center")
         self.textTeamBscore = self.c.create_text(self.canvas_width/4*3, self.canvas_height - 150, text="0", font=self.font_header, fill="white", justify="center")
 
         self.textTimer = self.c.create_text(self.canvas_width/2, 75, text="COUNTDOWN", font=self.font_header, fill="green", justify="center")
+
+        # register some mouse click events
+        # allow modification of votes by clicking on the vote numbers - right click to decrease
+        self.c.tag_bind(self.textTeamAscore,'<Button-1>', lambda event: self.registerVote(event,'a'));
+        self.c.tag_bind(self.textTeamAscore,'<Button-2>', lambda event: self.registerVote(event,'-a'));
+        self.c.tag_bind(self.textTeamAscore,'<Button-3>', lambda event: self.registerVote(event,'-a'));
+        self.c.tag_bind(self.textTeamBscore,'<Button-1>', lambda event: self.registerVote(event,'b'));
+        self.c.tag_bind(self.textTeamBscore,'<Button-2>', lambda event: self.registerVote(event,'-b'));
+        self.c.tag_bind(self.textTeamBscore,'<Button-3>', lambda event: self.registerVote(event,'-b'));
 
         self.status = 1;
         self.loadVote(self.currentVoteId - 1)
@@ -105,6 +121,17 @@ class Joypadui:
         self.countdownTimer()      
 
         self.c.pack()
+        
+    # register vote - manual overriding by clicking ... 
+    def registerVote(self, event, team):
+        if (team=='a'):
+            self.io.scoreA += 1
+        if (team=='-a'):
+            self.io.scoreA -= 1            
+        if (team=='b'):
+            self.io.scoreB += 1
+        if (team=='-b'):
+            self.io.scoreB -= 1            
         
     def loadVote(self, index):
 
@@ -157,6 +184,22 @@ class Joypadui:
             else:
                 self.c.itemconfig(self.imageGameB, image=self.photo_bb)
         
+        # reset hidden/shown states, position
+        self.c.itemconfig(self.textTeamAscore, state=NORMAL)
+        self.c.itemconfig(self.textTeamBscore, state=NORMAL)
+        self.c.itemconfig(self.textHeadingA, state=NORMAL)
+        self.c.itemconfig(self.textHeadingB, state=NORMAL)
+        self.c.itemconfig(self.textTimer, state=NORMAL)
+        self.c.coords(self.textHeadingA, self.canvas_width/4,self.heading_top)
+        self.c.coords(self.textHeadingB, self.canvas_width/4*3,self.heading_top)
+        if (hasattr(self,'imageGameA')):
+            self.c.itemconfig(self.imageGameA, state=NORMAL)
+            self.c.coords(self.imageGameA, self.canvas_width/4, self.canvas_height/2)
+        if (hasattr(self,'imageGameB')): 
+            self.c.itemconfig(self.imageGameB, state=NORMAL)
+            self.c.coords(self.imageGameB, self.canvas_width/4*3, self.canvas_height/2)
+                    
+        
     def countdownTimer(self):
         if (self.timeRemaining <= 0):
             self.timerReached();
@@ -190,18 +233,39 @@ class Joypadui:
                 print " ** "+ self.voteConfig[self.currentVoteId-1]['heading_b']+ " **";
                 self.announceWinner('b')
 
-        if (self.status == 2):
-            self.resetAndLoadNextVote()
-
+        
 
     def suddenDeathMode(self):
         self.status = 3
         self.root.after(100,self.timerReached);
 
     def announceWinner(self,team):
-        'something'
         self.status = 2
-        self.timerReached();
+        if (team=='-'):
+            if (self.displayTimeout <= 0):
+                self.resetAndLoadNextVote()
+                return
+        if (team=='a'):
+            self.c.itemconfig(self.bg, image=self.photoBG_win_a)
+            self.displayTimeout = self.timeOnVoteResults
+            self.c.coords(self.imageGameA, self.canvas_width/2, self.canvas_height/2)
+            self.c.coords(self.textHeadingA, self.canvas_width/2,self.heading_top)
+            self.c.itemconfig(self.imageGameB, state=HIDDEN)
+            self.c.itemconfig(self.textHeadingB, state=HIDDEN)
+        if (team=='b'):
+            self.c.itemconfig(self.bg, image=self.photoBG_win_b)
+            self.displayTimeout = self.timeOnVoteResults      
+            self.c.coords(self.imageGameB, self.canvas_width/2, self.canvas_height/2)
+            self.c.coords(self.textHeadingB, self.canvas_width/2,self.heading_top)
+            self.c.itemconfig(self.imageGameA, state=HIDDEN)
+            self.c.itemconfig(self.textHeadingA, state=HIDDEN)
+        if (team == 'a' or team == 'b'):
+            self.c.itemconfig(self.textTeamAscore, state=HIDDEN)
+            self.c.itemconfig(self.textTeamBscore, state=HIDDEN)
+            self.c.itemconfig(self.textTimer, state=HIDDEN)
+            
+        self.displayTimeout -= 1
+        self.root.after(1000, self.announceWinner, '-');
 
     def resetVote(self):
         self.status = 1
